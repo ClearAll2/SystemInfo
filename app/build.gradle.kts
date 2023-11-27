e8a146b42df6d94b293fc5983343554638556626
Đức Nguyễn <nguyenduc1021996@gmail.com>
2023-11-27 07:24:59 +0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val VERSION_CODE = 12
-val VERSION_NAME = "1.5.0"
+val VERSION_CODE = 13
+val VERSION_NAME = "1.5.1"
 
 android {
     namespace = "com.lkonlesoft.displayinfo"
@@ -56,10 +56,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.20"))
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.8.1")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
